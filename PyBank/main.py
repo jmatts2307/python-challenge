74bd901a2831ced5d6927feb4aeacc8a80aa81a2
jmatts2307 <jmatts2307@gmail.com>
2024-10-26 16:06:16 -0500

    changed absolute paths to relative paths

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,8 +3,8 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join("/Users/jesusmatajr/Desktop/python-challenge/PyBank/Resources/budget_data.csv")  # Input file path
-file_to_output = os.path.join("/Users/jesusmatajr/Desktop/python-challenge/PyBank/analysis/budget_analysis.txt")  # Output file path
+file_to_load = os.path.join("Resources", "budget_data.csv")  # Input file path
+file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
 
 # Define variables to track the financial data
 total_months = 0
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,9 +3,9 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-#I had to put the literal path as 
-file_to_load = os.path.join("/Users/jesusmatajr/Desktop/python-challenge/PyPoll/Resources/election_data.csv")  # Input file path
-file_to_output = os.path.join("/Users/jesusmatajr/Desktop/python-challenge/PyPoll/analysis/election_analysis.txt")  # Output file path
+#I used relative paths to make easier to run 
+file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
+file_to_output = os.path.join("analysis", "election_analysis.txt")  # Output file path
 
 # Initialize variables to track the election data
 total_votes = 0  # Track the total number of votes cast
